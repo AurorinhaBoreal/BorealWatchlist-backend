6182366912506f4cfed887068456071d833a0d59
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-03-13 22:11:53 -0300

    Server Config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("BackEnd ta rodando!")
+import express from "express"
+
+const server = express()
+
+server.listen(3333, () => {
+    console.log("The server is running!!")
+})
+
+
+// Documentação do Express:
+// https://expressjs.com
